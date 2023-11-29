c25912a74ff663b77fbaf7e128be3600c1aed3d0
nxtdxve <zettlerd@bzz.ch>
2023-11-29 13:57:40 +0100

    added testcases

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -4,4 +4,16 @@ describe('test suite traffic light', () => {
     it(`case red light`, () => {
         expect(app.trafficLight("red")).toBe("DON'T WALK!");
     });
+    it(`case orange light`, () => {
+        expect(app.trafficLight("orange")).toBe("ATTENTION!");
+    });
+    it(`case green light`, () => {
+        expect(app.trafficLight("green")).toBe("WALK!");
+    });
+    it(`case undefined light`, () => {
+        expect(app.trafficLight()).toBe("OUT OF ORDER");
+    });
+    it(`case other light`, () => {
+        expect(app.trafficLight("other")).toBe(undefined);
+    });
 });
